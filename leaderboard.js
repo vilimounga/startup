c8b5299e47b155c10d5c470b80f963cc85bc57d8
Viliami Mounga <mounga@gmail.com>
2024-04-17 18:15:55 -0600

    add loadScores function

---
+++ leaderboard.js
@@ -0,0 +1,50 @@
+function loadScores() {
+    let recordedData = [];
+    const recordedDataText = localStorage.getItem('recordedData');
+    if (recordedDataText) {
+        recordedData = JSON.parse(recordedDataText);
+    }
+
+    // Retrieve the username from localStorage
+    const userName = localStorage.getItem('userName');
+
+    console.log("User Name:", userName);
+
+    // Sort recorded data based on number of steps in descending order
+    recordedData.sort((a, b) => b.steps - a.steps);
+
+    const tableBodyEl = document.querySelector('.table tbody');
+
+    if (recordedData.length) {
+        for (const [i, data] of recordedData.entries()) {
+            const positionTdEl = document.createElement('td');
+            const nameTdEl = document.createElement('td');
+            const stepsTdEl = document.createElement('td');
+            const dateTdEl = document.createElement('td');
+
+            positionTdEl.textContent = i + 1;
+            nameTdEl.textContent = userName; // Use the username retrieved from localStorage
+            stepsTdEl.textContent = data.steps;
+
+            // Generate current date
+            const currentDate = new Date();
+            const options = { month: 'long', day: 'numeric', year: 'numeric' };
+            const formattedDate = currentDate.toLocaleDateString('en-US', options);
+            dateTdEl.textContent = formattedDate;
+
+            const rowEl = document.createElement('tr');
+            rowEl.appendChild(positionTdEl);
+            rowEl.appendChild(nameTdEl);
+            rowEl.appendChild(stepsTdEl);
+            rowEl.appendChild(dateTdEl);
+
+            tableBodyEl.appendChild(rowEl);
+        }
+    } else {
+        tableBodyEl.innerHTML = '<tr><td colspan="4">Be the first to record your data</td></tr>';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadScores();
+});
